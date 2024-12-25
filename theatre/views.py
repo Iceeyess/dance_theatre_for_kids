8873e97d01c30a73b224b38bf0b1ea3cf5b953fd
Dmitriy Krasheninnikov <ice_eyes@mail.ru>
2024-12-25 23:02:03 +0300

    schedule.html, header.html, urls.py and views.py in theatre are modified.

--- theatre/views.py
+++ theatre/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
 
+from theatre.models import RegularClassSchedule
+
+
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+def schedule(request):
+    data = dict(regular_class_schedule=RegularClassSchedule.objects.all())
+    return render(request, 'schedule.html', context=data)
